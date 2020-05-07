5e238a9c73db3296b2039d38f503741b45e57593
Arun_Suresh <31808179+asuresh213@users.noreply.github.com>
2020-05-07 01:24:32 -0700

    changed setup file

--- setup.py
+++ setup.py
@@ -1,27 +1,28 @@
 from distutils.core import setup
 setup(
-  name = 'NApy',         # How you named your package folder (MyLib)
-  packages = ['NApy'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+  name = 'NApy',        
+  packages = ['NApy'],
+  version = '0.1',
+  license='MIT',
   description = 'Python package for numerical analysis. Includes a variety of niche ode solvers and iterative matrix methods (typically encountered in a standard senior year course in numerical analysis)',   # Give a short description about your library
-  author = 'Arun Suresh',                   # Type in your name
-  author_email = 'ab251098@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/asuresh213/NApy',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/asuresh213/NApy/archive/v_01.tar.gz',    # I explain this later on
+  author = 'Arun Suresh',
+  author_email = 'ab251098@gmail.com',
+  url = 'https://github.com/asuresh213/NApy',
+  download_url = 'https://github.com/asuresh213/NApy/archive/v_01.tar.gz',
   keywords = ['ODE Solvers', 'Numerical', 'Numerical Analysis', 'Optimization',
-              'Matrix Methods', 'ODE', 'Differential Equations', 'Iterative matrix methods'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
+              'Matrix Methods', 'ODE', 'Differential Equations', 'Iterative matrix methods'],
+  install_requires=[
           'numpy',
           'tabulate',
           'matplotlib',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2.7'
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
